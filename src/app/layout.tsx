0ed2e62fae491261611f187bc33473ff160a8eab
Artur <metallist15431543@gmail.com>
2026-09-17 14:49:54 +0300

    feat: add RTK store and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { StoreProvider } from "@/shared/store/StoreProvider";
 
 export const metadata: Metadata = {
   title: "ShopHub",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
